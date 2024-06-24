56c07e8e10dc666449546146f5394efc49a283ef
Edgar Azevedo <edgarvazevedo@gmail.com>
2024-06-24 11:19:11 +0100

    feat: create alert dialog

---
+++ src/app/alert-dialog/alert-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-alert-dialog',
+  standalone: true,
+  templateUrl: './alert-dialog.component.html',
+  imports: [MatDialogModule, MatButtonModule]
+})
+export class AlertDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<AlertDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { message: string }
+  ) {}
+
+  onClose(): void {
+    this.dialogRef.close();
+  }
+}
